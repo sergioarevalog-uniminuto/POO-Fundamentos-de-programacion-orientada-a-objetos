2d0a48b3bd2c922023cc4968aca4be2cefffb810
sergioarevalog-uniminuto <sergio.arevalo-g@uniminuto.edu.co>
2026-09-16 21:26:47 -0500

    Create javaapplication.java

---
+++ Parcial/javaapplication.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication;
+
+public class Empleado {
+
+    public static void main(String[] args) {
+        Empleado empleado1 = new Empleado();
+        
+    }
+    
+}
